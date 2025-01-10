4a7dc68156ca2ca6378bca11790b03390e4f0caf
Jefri <22081447@students.latrobe.edu.au>
2025-01-11 00:00:35 +0700

    testpage fixing

--- app/testpage/page.jsx
+++ app/testpage/page.jsx
@@ -1,5 +1,4 @@
 import BrandCategory from "@/component/visualizations/BrandCategory";
-import ProjectStatusChart from "../visualizations/project/page";
 import ProjectProgress from "@/component/visualizations/ProjectProgress";
 
 export default function TestPage() {

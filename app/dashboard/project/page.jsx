3c63e28464b7a10ebef26e829dd7db2fd6d0ed48
MONAHIKOS <billaqn1202@gmail.com>
2025-01-15 03:00:55 +0700

    Adding year 2025 in the filter by years

--- app/dashboard/project/page.jsx
+++ app/dashboard/project/page.jsx
@@ -290,6 +290,7 @@ const Tracker = () => {
               className="p-2 border border-gray-300 rounded-lg"
             >
               <option value="all">All Years</option>
+              <option value="2025">2025</option>
               <option value="2024">2024</option>
               <option value="2023">2023</option>
               <option value="2022">2022</option>

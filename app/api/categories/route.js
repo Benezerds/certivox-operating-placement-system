1a5f670f05456b3cd854ce6da7f961af85f55b3f
Benaya A.S <84769509+Benezerds@users.noreply.github.com>
2025-01-14 11:34:31 +0700

    Added PUT and DELETE request to the api/categories route

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -1,5 +1,5 @@
 // app/api/projects/route.js
-import { addCategory,getAllCategories, getCategoriesByDocRef } from '../../../lib/categoriesService';
+import { addCategory,getAllCategories, getCategoriesByDocRef, updateCategory, deleteCategory  } from '../../../lib/categoriesService';
 
 export async function GET(request) {
   try {
@@ -17,5 +17,41 @@ export async function POST(request) {
     return new Response(JSON.stringify(newCategory), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ error: 'Server error' }), { status: 500 });
+  }
+}
+
+export async function PUT(request) {
+  try {
+    const url = new URL(request.url);
+    const categoryId = url.searchParams.get('id'); // Extract the category ID from the query params
+    const updatedData = await request.json();
+
+    if (!categoryId) {
+      return new Response(JSON.stringify({ error: 'Category ID is required' }), { status: 400 });
+    }
+
+    const updatedDataResult = await updateCategory(categoryId, updatedData);
+    return new Response(JSON.stringify(updatedDataResult), { status: 200 });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: 'Failed to update data, Server Error' }),
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request) {
+  try {
+    const url = new URL(request.url);
+    const categoryId = url.searchParams.get('id'); // Extract the category ID from the query params
+
+    if (!categoryId) {
+      return new Response(JSON.stringify({ error: 'Category ID is required' }), { status: 400 });
+    }
+
+    const deleteResult = await deleteCategory(categoryId);
+    return new Response(JSON.stringify(deleteResult), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: 'Failed to delete data, Server Error' }), { status: 500 });
   }
 }
